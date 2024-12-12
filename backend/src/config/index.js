7cfa049a8f601af3f6c45078e9525491a3020461
Alpha Lencho <Alphalenchoo@gmail.com>
2024-12-12 16:35:21 +0300

    Update index.js

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.PG_DB,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
+    ssl: {
+        rejectUnauthorized: false, // Allows SSL without strict certificate validation
+      },
 });
 
 module.exports = pool;
